65e73991273a16ecee0f569c762fc7ec2ff4a24d
gabrielaquintero <88777969+gabrielaquintero@users.noreply.github.com>
2022-03-02 19:06:15 -0500

    cuarta parte

--- Gabriela.js
+++ Gabriela.js
@@ -1,6 +1,6 @@
 'use strict'
 
-/*Dada una cadena de texto (string) separe y muestre en consola los caracteres de forma desordenada uno por línea, 1 caracter a la vez.*/
+/*1.Dada una cadena de texto (string) separe y muestre en consola los caracteres de forma desordenada uno por línea, 1 caracter a la vez.*/
 
 let testWord = "esternocleidomastoideo";
 function shuffle(a,b) {
@@ -17,7 +17,7 @@ split.forEach(letter=>console.log(letter))
 }
 wordCutter(testWord)
 
-/*Dado un string buscar en un listado e indicar si se encuentra o no
+/*2.Dado un string buscar en un listado e indicar si se encuentra o no
 ahí contenido, debe soportar mayúsculas y minúsculas sin importar
 la variación, es lo mismo Carro, CARRO o carro.*/
 
@@ -42,7 +42,7 @@ function wordSearcherIgnoreCase(targetWord, wordsList) {
 
 
 
-/*Dado un arreglo de strings, retornar la palabra más larga,
+/*3.Dado un arreglo de strings, retornar la palabra más larga,
 la más corta y el tamaño promedio, el arreglo debe ser
 entregado por parámetro y puede variar en cantidad de palabras
 del arreglo de entrada libremente, debe retornar un objeto
@@ -58,12 +58,29 @@ let testSampleList = [
     "Zar",
 ];
 
-function wordLengthClassifier(wordsList) {
-    // :)
+function wordLengthClassifier(words) {
+    let totalLetters=0
+    const sortedWords=words.sort((wordA,wordB)=>{
+        if(wordA.length<wordB.length){
+            return -1
+        } else if (wordA.length>wordB.length){
+            return 1
+        } else{
+            return 0
+        }
+    })
+    sortedWords.forEach(word=>totalLetters+=word.length)
+    const average=totalLetters/sortedWords.length
+    return{
+        shortestWord: sortedWords[0],
+        longestWord: sortedWords[sortedWords.length-1],
+        average
+    }
 }
+console.log(wordLengthClassifier(testSampleList))
 
 
-/*Dado un string retorna si este es o no un palíndromo. No debe diferenciar entre mayúsculas y minúsculas*/
+/*4.Dado un string retorna si este es o no un palíndromo. No debe diferenciar entre mayúsculas y minúsculas*/
 
 let onVerificationWordA = "reconocer";
 let onVerificationWordB = "querer";
@@ -75,7 +92,7 @@ function palindromeVerifier(word) {
 }
 
 
-/*Dado un objeto que contiene una lista de palabras contar el
+/*5.Dado un objeto que contiene una lista de palabras contar el
 número de letras vocales y consonantes y retornarlo en un arreglo de 2 posiciones.*/
 let containerTestObject = {
     list:["Cumbamba", "Oreja", "Nariz", "Ojo", "Lengua", "Diente"]
@@ -100,7 +117,7 @@ console.log(lettersCounter(containerTestObject))
 
 
 
-/*Dado 2 arreglos de strings retornar un arreglo con todos los strings.*/
+/*6.Dado 2 arreglos de strings retornar un arreglo con todos los strings.*/
 let wordArrayA = ["hola", "¿" ,"cómo", "estás", "?"];
 let wordArrayB = ["te", "ves" ,"igual", "te", "ves", "igual"];
 
@@ -111,17 +128,25 @@ function arrayJoiner(listA, listB) {
 }
 arrayJoiner()
 
-/*Dado un arreglo de strings indicar qué posiciones del arreglo
+/*7.Dado un arreglo de strings indicar qué posiciones del arreglo
 son anagramas de una palabra base (recibida como parámetro), retorne las posiciones en un arreglo.*/
 
 let testWordToExplore = "amar";
 let wordsToVerify = ["amar", "arma", "rana" , "mara", "rama", "roma", "amor", "ramon", "omar"];
 
 function anagramVerifier(wordToExplore, listOfWords) {
-   
+   array=[]
+   wordsToVerify.forEach((palabra,index) =>{
+       if(palabra.split('').every(letra=>{
+           return testWordToExplore.includes(letra)
+       })) 
+       array.push(index)
+    })
+    return array
 }
+console.log(anagramVerifier(testWordToExplore, wordsToVerify))
 
-/*Dado un objeto que contiene 2 arreglos, retornar un objeto con 1
+/*8.Dado un objeto que contiene 2 arreglos, retornar un objeto con 1
 arreglo que contiene las palabras sin vocales.*/
 
 let testObjMultiContainer = {
@@ -148,7 +173,7 @@ vocalsRemoverFromObject(testObjMultiContainer)
 
 console.log(vocalsRemoverFromObject(testObjMultiContainer));
 
-/*Dado un arreglo de palabras reemplazar la última vocal por una x y retornar dicho arreglo.*/
+/*9.Dado un arreglo de palabras reemplazar la última vocal por una x y retornar dicho arreglo.*/
 
 let someWordsToTest = ["compañeros", "estudiantes", "señores", "amigos", "graduandos", "artistas", "universitarios"];
 
@@ -157,7 +182,7 @@ function lastVocalReplacer(words) {
 }
 
 
-/*Dada una lista de palabras verificar si alguna de las palabras es la
+/*10.Dada una lista de palabras verificar si alguna de las palabras es la
 versión al revés de alguna de las palabras de una segunda lista,
 debe contar las identificadas y retornar un objeto con ese conteo.*/
 
